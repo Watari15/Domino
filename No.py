c1bb32dcab41c995f0a063d679f9ba1c38d2a3cf
Watari15 <watari15002@hotmail.com>
2024-10-11 10:50:48 +0200

    Update No.py

--- No.py
+++ No.py
@@ -18,6 +18,15 @@ class Domino():
         nbr_point = self.val1 + self.val2
     def affichage(self):
         if self.double(self)==True:
-            print(" ---\n|" + self.val1 + "|\n---\n|" + self.val2 + "|\n---")
+            print(" ---\n|" + str(self.val1) + "|\n---\n|" + str(self.val2) + "|\n---")
         else:
             print("---------\n| "+ str(self.val1) +" | "+ str(self.val2) +" |\n---------")
+    def __getgauche__(self):
+        return self.val1
+    def __getdroite__(self):
+        return self.val1
+    def __setgauche__(self,nouvellegauche):
+        self.val1 = nouvellegauche
+    def __setdroite__(self,nouvelledroite):
+        self.val2 = nouvelledroite
+    
